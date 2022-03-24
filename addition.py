0c954f44e8594f66edcd9e8ff0fd1b8ac5492a53
Tapan Roul <troul76023@gmail.com>
2022-03-24 11:52:32 +0530

    data type error eliminated

--- addition.py
+++ addition.py
@@ -1,3 +1,4 @@
-a=input("Give the 1st number")
-b=input("Give the 2nd number")
-print(x=a+b)
+a=int(input("Give the 1st number\n"))
+b=int(input("Give the 2nd number\n"))
+print("The sum =" , a+b)
+
