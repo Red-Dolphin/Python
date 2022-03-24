22a251d1519ae765add7fd3744b86075dad539e0
Tapan Roul <troul76023@gmail.com>
2022-03-24 15:51:41 +0530

    checking a number is positive or not by if and else statement

---
+++ If_and_else.py
@@ -0,0 +1,8 @@
+x=int(input("give your number\n"))
+if x==0 :
+    print("your number is neither positive nor negative")
+elif x>0 :
+    print('your number is positive')
+
+else :
+    print("your number is negative")    
