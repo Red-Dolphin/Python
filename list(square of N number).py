c0787cda721e3beee282601eb76bb7175b1079a0
Tapan Roul <troul76023@gmail.com>
2022-03-29 19:36:53 +0530

    List of square of N number

---
+++ list(square of N number).py
@@ -0,0 +1,18 @@
+# square root of natural number
+
+
+
+
+n=int(input("Give how many numbe you want\n"))
+if n<0 :
+    print('sorry please give positive number')
+square=[]
+for x in range(1,n):
+    square.append(x**2)
+print(square)  
+total=0                         # Don't put this inside for loop
+for y in range(0,(n-1)):
+
+             # total=0 The sum will returns to the last value of the list 
+    total=total +  square[y]
+print(total)    
